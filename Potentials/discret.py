13e842d22162d22fd697bb609f0250d4106d4e85
Re3syyy <baptiste.desmedt@icloud.com>
2025-04-04 15:52:57 +0200

    Fix : pot/discret for general cases

--- Potentials/discret.py
+++ Potentials/discret.py
@@ -9,12 +9,15 @@ def discretiser(x_min, x_max, num_intervalles, pot, params):
     V_bins = pot(x_centers, params)
     V_bins[0] = 0.0
     V_bins[-1] = 0.0
+    x_centers[0] = x_min
+    x_centers[-1] = x_max
     return x_bins, V_bins, x_centers
 
 def discretiser_period(x_min, x_max, num_intervalles, pot,L,N, params):
     x_bins = np.linspace(x_min, x_max, num_intervalles + 1)
     x_centers = (x_bins[:-1] + x_bins[1:]) / 2
-
+    x_centers[0] = x_min
+    x_centers[-1] = x_max
     V_bins = pot(x_centers,params,N,L)
     V_bins[0] = 0.0
     V_bins[-1] = 0.0
--- main.py
+++ main.py
@@ -16,16 +16,18 @@ def main():
     eps0 = 8.8541878188e-12
     x0 = L
     params = (Q, e, a,x0, eps0)
-    num_intervalles = 11
+    num_intervalles = 101
     x = np.linspace(x_min, x_max, 1000)
     E_min = -10 * 1.6e-19     # -10 eV
     E_max = -1e-20
+    x_bins, V_bins, x_centers = discretiser_period(x_min, x_max, num_intervalles,pot_period,L, N, params)
+    E_admited = find_eigenvalues(x_bins, V_bins, E_min, E_max, 1e-5)
 
     
     plt.figure(figsize=(12, 6))
     plt.plot(x,[pot_period(xi, params,N,L) for xi in x], label="Potentiel", color="green", linewidth=1)
-    #plt.step(x_bins, np.append(V_bins[0], V_bins), where='pre', label="Découpe", color="blue", linewidth=1)
-    #plt.scatter(x_centers , V_bins , color="red", linewidth=0.5, label="mesh")
+    plt.step(x_bins, np.append(V_bins[0], V_bins), where='pre', label="Découpe", color="blue", linewidth=1)
+    plt.scatter(x_centers , V_bins , color="red",s =15, linewidth=0.5, label="mesh")
     plt.xlabel("x [m]")
     plt.ylabel("V(x) [J]")
     plt.legend()
