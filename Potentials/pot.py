c9101df5ebe2b997d9968f5b63ae19961008dd9b
Re3syyy <baptiste.desmedt@icloud.com>
2025-04-10 19:59:17 +0200

    try to fix pot

--- Potentials/pot.py
+++ Potentials/pot.py
@@ -56,6 +56,52 @@ def pot_period(x, params, N, L):
     # Calcul du potentiel total
     V = np.zeros_like(x)
     for xi in x_wells:
-        V += - (Q * e) / (4 * np.pi * eps0) / np.sqrt((x - xi)**2 + a**2)
+        V += - (Q * e) / (4 * np.pi * eps0) *np.sqrt((x - xi)**2 + a**2)
+    return V
+
+def potential_voisin(x, params, N, L):
+    """
+    Calcule le potentiel périodique 1D pour un cristal avec N puits coulombiens adoucis,
+    en considérant uniquement le puits le plus proche de chaque position x.
+
+    Paramètres :
+    x : array_like      → Positions (en mètres) où calculer le potentiel.
+    Q : float           → Charge des noyaux (en multiples de la charge élémentaire e).
+    e : float           → Charge élémentaire (1.602e-19 C).
+    epsilon_0 : float   → Permittivité du vide (8.854e-12 F/m).
+    a : float           → Paramètre d'adoucissement (en mètres).
+    L : float           → Distance entre les puits (en mètres).
+    N : int             → Nombre de puits.
     
+    Retourne :
+    V : ndarray         → Potentiel aux positions x (en volts).
+    """
+    
+    Q, e, a, x0, eps0 = params
+    x = np.asarray(x)
+    V = np.zeros_like(x)
+    x_wells = x0 + np.arange(N) * L  # Positions des N puits
+    
+
+    if x <= x0 : 
+        d = np.sqrt((x-x0)**2 + a**2)
+        V = - (Q * e)/ (4 * np.pi * eps0 * d)
+
+    elif x >=  N*L :
+        d = np.sqrt((x - N*L)**2 + a**2)
+        V = - (Q * e)/ (4 * np.pi * eps0 * d)
+
+    else : 
+        k_floor = np.floor((x - x0) / L).astype(int)
+        left_well = np.clip(k_floor, 0, N-1)
+
+        right_well = np.clip(k_floor + 1, 0, N-1)
+        x_left = x_wells[left_well]
+        dl = np.sqrt((x-x_left)**2 + a**2)
+        x_right = x_wells[right_well]
+        dr = np.sqrt((x-x_right)**2 + a**2)
+        if dr < dl : 
+            V -=   (Q * e) / (4 * np.pi * eps0 * dr)
+        else :
+            V -=  (Q * e) / (4 * np.pi * eps0 * dl)
     return V
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from Potentials import discretiser, pot, pot_period, discretiser_period
+from Potentials import discretiser, pot, pot_period, discretiser_period, potential_voisin
 from Solvers import solve, build_transfer_matrix, find_eigenvalues
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 def main(): 
     #constante :
     L = 0.5e-9
-    N = 1
+    N = 2
     x_min = 0.0
     x_max = L*N+ L
     a = 0.1e-9
@@ -22,23 +22,23 @@ def main():
     E_max = -1e-20
     E = -10*6*1.6e-19
     S0 = 1e-3
-    x_bins, V_bins, x_centers = discretiser_period(x_min, x_max, num_intervalles,pot_period,L, N, params)
+    #x_bins, V_bins, x_centers = discretiser_period(x_min, x_max, num_intervalles,pot_period,L, N, params)
     k = np.pi / L 
 
-    sol = solve(pot_period, E, S0,x,params,N,L,k)
-    
+    #sol = solve(pot_period, E, S0,x,params,N,L,k)
+    x_wells = x0 + np.arange(N) * L
 
     
-    plt.figure(figsize=(12, 6))
+    plt.figure(figsize=(14, 6))
     #plt.plot(x,[pot_period(xi, params,N,L) for xi in x], label="Potentiel", color="green", linewidth=1)
+    plt.plot(x,[potential_voisin(xi, params,N,L) for xi in x], label="Potentiel 1er voisin", color="blue", linewidth=1)
     #plt.step(x_bins, np.append(V_bins[0], V_bins), where='pre', label="Découpe", color="blue", linewidth=1)
-    #plt.scatter(x_centers , V_bins , color="red",s =15, linewidth=0.5, label="mesh")
-    #plt.xlabel("x [m]")
-    #plt.ylabel("V(x) [J]")
-    plt.plot(x, sol.sol(x)[0], label='Solution numérique')
+    plt.xlabel("x [m]")
+    plt.ylabel("V(x) [J]")
+    #plt.plot(x, sol.sol(x)[0], label='Solution numérique')
     plt.xlabel("x [m]")
     plt.ylabel("psi(x)**2 [m]")
-    plt.legend()
+    plt.legend(loc = "best")
     plt.show()  
 if __name__ == "__main__":
     main()
